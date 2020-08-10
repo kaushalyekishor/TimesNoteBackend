eac1bf85afc0ff43100f66342623682a40bfb5d8
Kishor <kaushalyekishor1232gmail.com>
2020-08-10 18:19:02 +0530

    times note code added

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ var app=express();
 const database=require('./config/database.config');
 var userRoute=require('./app/routes/user.route')
 const noteRoute=require('./app/routes/note.route')
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    });
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/user',userRoute);
